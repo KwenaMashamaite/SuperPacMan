e0e865b0fed85614b748e578bc96950787746dde
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2021-06-09 15:12:19 +0200

    Hide console window in release build

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include "src/models/game/Game.h"
 
+#ifdef NDEBUG
+    #include "windows.h"
+#endif
+
 int main() {
+
+    // Hide console window in release mode
+#ifdef NDEBUG
+    HWND hwnd = GetConsoleWindow();
+    ShowWindow(hwnd, SW_HIDE);
+#endif
+
     auto superPacMan = spm::Game();
     superPacMan.initialize();
     superPacMan.start();
